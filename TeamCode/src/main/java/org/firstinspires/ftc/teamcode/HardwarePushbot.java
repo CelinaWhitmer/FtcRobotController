c5f34870d5ac139100fcf491e45af7346d0c5052
Celina Whitmer <celinawhitmer@Daves-MacBook-Pro.local>
2021-07-24 17:10:14 -0400

    Changed center arm to right arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwarePushbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwarePushbot.java
@@ -55,7 +55,7 @@ public class HardwarePushbot
     /* Public OpMode members. */
     public DcMotor  leftDrive   = null;
     public DcMotor  rightDrive  = null;
-    public DcMotor  centerArm     = null;
+    public DcMotor  rightArm     = null;
     public Servo    leftClaw    = null;
     public Servo    rightClaw   = null;
 
@@ -80,20 +80,20 @@ public class HardwarePushbot
         // Define and Initialize Motors
         leftDrive  = hwMap.get(DcMotor.class, "left_drive");
         rightDrive = hwMap.get(DcMotor.class, "right_drive");
-        centerArm    = hwMap.get(DcMotor.class, "centrer_arm");
+        rightArm    = hwMap.get(DcMotor.class, "right_arm");
         leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
 
         // Set all motors to zero power
         leftDrive.setPower(0);
         rightDrive.setPower(0);
-        centerArm.setPower(0);
+        rightArm.setPower(0);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
         leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        centerArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         // Define and initialize ALL installed servos.
         leftClaw  = hwMap.get(Servo.class, "left_hand");
